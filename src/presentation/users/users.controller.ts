fc71766cb576d97924dca8e90ab084d4788ca333
Rafael Mançan <rafael.mancan@gmail.com>
2022-11-17 17:52:43 -0300

    feat(presentation): users methods

---
+++ src/presentation/users/users.controller.ts
@@ -0,0 +1,26 @@
+import { Body, Controller, Get, Post } from "@nestjs/common";
+import { Observable } from "rxjs";
+
+import { UserCreateDto } from "src/shared/dtos/user-create.dto";
+import { UserCreatedDto } from "src/shared/dtos/user-created.dto";
+import { CreateUserUseCase } from "src/use-cases/users/create-user.usecase";
+import { GetAllUsersUseCase } from "src/use-cases/users/get-all-users.usecase";
+
+@Controller("/users")
+export class UsersControllers {
+
+  constructor(
+    private createUserUserCase: CreateUserUseCase,
+    private getAllUsersUserCase: GetAllUsersUseCase
+  ) { }
+
+  @Post()
+  public create(@Body() user: UserCreateDto): Observable<UserCreatedDto> {
+    return this.createUserUserCase.execute(user);
+  }
+
+  @Get()
+  public findAll(): Observable<UserCreatedDto[]> {
+    return this.getAllUsersUserCase.execute()
+  }
+}
